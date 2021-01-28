a68ce3f834478724e9e9bb4b5e832c2eb5e5e744
igniterab <ashishbhardwajexcel@gmail.com>
2021-01-28 12:15:53 +0530

    all the new updates have been changed

--- myfitness/myfitness/settings.py
+++ myfitness/myfitness/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import dj_database_url #for heroku
+# import dj_database_url  # for heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,7 +26,7 @@ SECRET_KEY = 'qnq+%s1rjd#stnc5zj5*s(g^(sb&1p+01=9-fda1)&c%ae_bny'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['http://myfitness13.herokuapp.com/']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # 'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -120,17 +120,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-#for local host
-'''
+# for local host
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 MEDIA_URL = '/media/profiles/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/profiles')
+
+
 '''
-
-
 #///////////////////////////////////////////
 #for heroku use this
 PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
@@ -147,3 +147,5 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
+
+'''
